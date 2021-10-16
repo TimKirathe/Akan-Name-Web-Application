de97b64e2ea022705d55eb0d35f4efd857cf1f4b
Timothy Kirathe <timothykirathe@Timothys-MacBook-Pro.local>
2021-10-16 23:10:41 +0300

    Completion of dayOfBirth calculation in javascript file

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,31 @@ function dateInput() {
   let dobSliced2 = parseInt(dob.value.slice(2,4));
   let dobSliced3 = parseInt(dob.value.slice(5,7));
   let dobSliced4 = parseInt(dob.value.slice(8,10));
-  (((dobSliced1/4)-2*dobSliced1-1)+((5*dobSliced2/4))+((26*(dobSliced3+1)/10))+DD)%7
+  var day = (((dobSliced1/4)-2*dobSliced1-1)+((5*dobSliced2/4))+((26*(dobSliced3+1)/10))+dobSliced4)%7
+  var dayOfBirth = parseInt(day.toFixed());
+  var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  if (dayOfBirth===0) {
+  alert("You were born on a " + daysOfWeek[0] + "!")
+}
+  else if (dayOfBirth===1) {
+  alert("You were born on a " + daysOfWeek[1] + "!")
+}
+  else if (dayOfBirth===2) {
+  alert("You were born on a " + daysOfWeek[2] + "!")
+}
+  else if (dayOfBirth===3) {
+  alert("You were born on a " + daysOfWeek[3] + "!")
+}
+  else if (dayOfBirth===4) {
+  alert("You were born on a " + daysOfWeek[4] + "!")
+}
+  else if (dayOfBirth===5) {
+  alert("You were born on a " + daysOfWeek[5] + "!")
+}
+  else if (dayOfBirth===6) {
+  alert("You were born on a " + daysOfWeek[6] + "!")
+  }
+  else {
+    alert("Wrong input, please enter a valid date of birth.")
+  }
 }
