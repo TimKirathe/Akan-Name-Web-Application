2a78df9fb89f252f4688f777b11f27d5478ce368
Timothy Kirathe <timothykirathe@Timothys-MacBook-Pro.local>
2021-10-17 21:45:01 +0300

    Finalization of Akan Name Output taking in the dateOfBirth and gender type. Used one function encapsulating all different variables and methods to be executed when called

--- js/scripts.js
+++ js/scripts.js
@@ -7,32 +7,43 @@ function Input() {
   var day = (((dobSliced1/4)-2*dobSliced1-1)+((5*dobSliced2/4))+((26*(dobSliced3+1)/10))+dobSliced4)%7;
   var dayOfBirth = parseInt(day.toFixed());
   var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  if (dayOfBirth===0) {
-  document.getElementById("dayOfBirthOutput").innerHTML = "You were born on a " + daysOfWeek[0] + "!"
+  var genderInput = document.getElementById("genderInput");
+  let gender = genderInput.value.charAt(0).toUpperCase() + genderInput.value.slice(1);
+  var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  if (gender==="Male" && dayOfBirth===0) {
+    document.getElementById("akanNameOutput").innerHTML ="You were born on a " + daysOfWeek[0] + " and your Akan Name is " + maleAkanNames[0] + "!"
 }
-  else if (dayOfBirth===1) {
-  document.getElementById("dayOfBirthOutput").innerHTML = "You were born on a " + daysOfWeek[1] + "!"
+  else if (gender==="Male" && dayOfBirth===1) {
+    document.getElementById("akanNameOutput").innerHTML ="You were born on a " + daysOfWeek[1] + " and your Akan Name is " + maleAkanNames[1] + "!"
 }
-  else if (dayOfBirth===2) {
-  document.getElementById("dayOfBirthOutput").innerHTML = "You were born on a " + daysOfWeek[2] + "!"
+  else if (gender==="Male" && dayOfBirth===2) {
+    document.getElementById("akanNameOutput").innerHTML ="You were born on a " + daysOfWeek[2] + " and your Akan Name is " + maleAkanNames[2] + "!"
 }
-  else if (dayOfBirth===3) {
-  document.getElementById("dayOfBirthOutput").innerHTML = "You were born on a " + daysOfWeek[3] + "!"
-}
-  else if (dayOfBirth===4) {
-  document.getElementById("dayOfBirthOutput").innerHTML = "You were born on a " + daysOfWeek[4] + "!"
-}
-  else if (dayOfBirth===5) {
-  document.getElementById("dayOfBirthOutput").innerHTML = "You were born on a " + daysOfWeek[5] + "!"
-}
-  else if (dayOfBirth===6) {
-  document.getElementById("dayOfBirthOutput").innerHTML = "You were born on a " + daysOfWeek[6] + "!"
+  else if (gender==="Male" && dayOfBirth===3) {
+    document.getElementById("akanNameOutput").innerHTML ="You were born on a " + daysOfWeek[3] + " and your Akan Name is " + maleAkanNames[3] + "!"
   }
+  else if (gender==="Male" && dayOfBirth===4) {
+    document.getElementById("akanNameOutput").innerHTML ="You were born on a " + daysOfWeek[4] + " and your Akan Name is " + maleAkanNames[4] + "!"
+}
+  else if (gender==="Male" && dayOfBirth===5) {
+    document.getElementById("akanNameOutput").innerHTML ="You were born on a " + daysOfWeek[5] + " and your Akan Name is " + maleAkanNames[5] + "!"
+}
+  else if (gender==="Male" && dayOfBirth===6) {
+    document.getElementById("akanNameOutput").innerHTML ="You were born on a " + daysOfWeek[6] + " and your Akan Name is " + maleAkanNames[6] + "!"
+}
+  else if (gender==="Female" && dayOfBirth===0) {
+    document.getElementById("akanNameOutput").innerHTML ="You were born on a " + daysOfWeek[0] + " and your Akan Name is " + femaleAkanNames[0] + "!"
+}
+  else if (gender==="Female" && dayOfBirth===1) {
+    document.getElementById("akanNameOutput").innerHTML ="You were born on a " + daysOfWeek[0] + " and your Akan Name is " + femaleAkanNames[0] + "!"
+}
+  else if (gender==="Female" && dayOfBirth===2) {}
+  else if (gender==="Female" && dayOfBirth===3) {}
+  else if (gender==="Female" && dayOfBirth===4) {}
+  else if (gender==="Female" && dayOfBirth===5) {}
+  else if (gender==="Female" && dayOfBirth===6) {}
   else {
-    alert("Wrong input, please enter a valid date of birth.")
+    alert("Wrong input, please enter valid information.")
   }
-  var gender = document.getElementById("genderInput");
-  var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-  var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-
 }
