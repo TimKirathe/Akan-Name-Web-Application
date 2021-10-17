5ba2ff4f38f78721db62b27b1eb21cbcc9266bdc
Timothy Kirathe <timothykirathe@Timothys-MacBook-Pro.local>
2021-10-17 19:30:24 +0300

    Complete change of gender input method. Both inputs are now executed under one button at the end of filling in the fields. Now have begun working on the gender input manipulization and offical akanName output

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-function dateInput() {
+function Input() {
   let dob = document.getElementById("dobInput");
   let dobSliced1 = parseInt(dob.value.slice(0,2));
   let dobSliced2 = parseInt(dob.value.slice(2,4));
   let dobSliced3 = parseInt(dob.value.slice(5,7));
   let dobSliced4 = parseInt(dob.value.slice(8,10));
-  var day = (((dobSliced1/4)-2*dobSliced1-1)+((5*dobSliced2/4))+((26*(dobSliced3+1)/10))+dobSliced4)%7
+  var day = (((dobSliced1/4)-2*dobSliced1-1)+((5*dobSliced2/4))+((26*(dobSliced3+1)/10))+dobSliced4)%7;
   var dayOfBirth = parseInt(day.toFixed());
   var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   if (dayOfBirth===0) {
@@ -31,10 +31,8 @@ function dateInput() {
   else {
     alert("Wrong input, please enter a valid date of birth.")
   }
-  function maleInput() {
-    var akanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    if (dayOfBirth===3) {
-      alert("Your Akan Name is " + akanNames[3] + "!")
-    }
-  }
+  var gender = document.getElementById("genderInput");
+  var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+  var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+
 }
